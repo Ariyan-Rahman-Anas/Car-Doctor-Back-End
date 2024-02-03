fee617a6a7fe86f759859cc3ca781af16d63660a
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-02-04 01:14:14 +0600

    Implemented the deleting functionality for myBooking route

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       res.send(result);
     });
 
+    //deleting item form myBookings cart
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollections.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
